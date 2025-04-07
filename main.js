28a767df3e5d91981bf35d82c73f8997e8e66dc7
Isabelly Mascarello <147404788+isaamascarello@users.noreply.github.com>
2025-04-07 11:27:30 -0300

    ok

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function calculaTempo(tempoObjetivo) {
     let dias = Math.floor(horas / 24);
 
     segundos %= 60;
-    minutos %= 68;
+    minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
@@ -43,17 +43,16 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-        document.getElementById("seg1").textContent = calculaTempo(tempos[1])[3];
-        document.getElementById("seg2").textContent = calculaTempo(tempos[2])[3];
-        document.getElementById("seg3").textContent = calculaTempo(tempos[3])[3];
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
 function comecaCronometro() {
-    for (let i = 0; i < tempos.length; i++) {
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
-      }
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
 }
 
 comecaCronometro();
