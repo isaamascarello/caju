11e463128bd7dbfc0c7bfa6d5a0a2b10107a37fd
Isabelly Mascarello <147404788+isaamascarello@users.noreply.github.com>
2025-04-07 11:21:34 -0300

    ok

--- main.js
+++ main.js
@@ -43,10 +43,10 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+        document.getElementById("seg1").textContent = calculaTempo(tempos[1])[3];
+        document.getElementById("seg2").textContent = calculaTempo(tempos[2])[3];
+        document.getElementById("seg3").textContent = calculaTempo(tempos[3])[3];
     }
 }
 
